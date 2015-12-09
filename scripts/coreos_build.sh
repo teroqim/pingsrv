1c6aef95c6c9bd87dc701efb64500bef3100e39a
CoreOS Admin <core@ip-10-0-0-175.us-west-2.compute.internal>
2015-12-09 14:10:05 +0000

    Updat build script

--- scripts/coreos_build.sh
+++ scripts/coreos_build.sh
@@ -4,15 +4,10 @@
 set -e
 
 #Update repository
-#git pull origin master
+git pull origin master
 
 #Build dockerfile
 docker build --rm -t $(etcdctl get /announce/services/docker-registry/host)/pingsrv:latest .
-
-#Restart docker (hack for now, since docker _always_ hangs when trying to push image immediately after building it)
-sudo systemctl restart docker
-
-#TODO: Look into using the timeout command for the push and if it fails, then restart docker. Loop this for a while and if it still fails then exit
 
 #Push dockerfile
 docker push $(etcdctl get /announce/services/docker-registry/host)/pingsrv:latest
